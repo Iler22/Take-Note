f36b71d34f109d85cb20a40de4625f502fa1aadf
Real Inspiration <ilerwatson22@gmail.com>
2022-01-23 16:01:59 +0000

    Api route for getting, posting and deleting notes.

---
+++ Routes/api-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const DbStore = require('../db/db-store');
+
+router.get('/notes', (req, res) => {
+  DbStore.getNote()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+  DbStore.addNewNote(req.body)
+    .then((note) => {
+      res.json(note);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/notes/:id', (req, res) => {
+  DbStore.deleteNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
