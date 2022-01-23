77426fcddf60efc663b1875f7c5f70a932eed30a
Real Inspiration <ilerwatson22@gmail.com>
2022-01-23 17:55:04 +0000

    Database store which receives notes as JSON with functionality to get note, add a new note and delete a note.

---
+++ db/db-store.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const util = require('util');
+const uniqueId = require('uuid').v4;
+const { workerData } = require('worker_threads');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class DbStore {
+  readNote() {
+    return readFileAsync('db/db.json', 'utf-8');
+  }
+
+  writeNote(note) {
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+
+  getNote() {
+    return this.readNote().then((notes) => {
+      let newNotes;
+
+      try {
+        newNotes = [].concat(JSON.parse(notes));
+      } catch (error) {
+        newNotes = [];
+      }
+
+      return newNotes;
+    });
+  }
+
+  addNewNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error('message');
+    }
+
+    const newNote = {
+      title,
+      text,
+      id: uniqueId(),
+    };
+
+    return this.getNote()
+      .then((notes) => [...notes, newNote])
+      .then((updateNote) => this.writeNote(updateNote))
+      .then(() => newNote);
+  }
+
+  deleteNote(id) {
+    return this.getNote()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((noteToDelete) => this.writeNote(noteToDelete));
+  }
+}
+
+module.exports = new DbStore();
