8c2cfc1f20c82d03a626abb1e4d264f1298d5431
Real Inspiration <ilerwatson22@gmail.com>
2022-01-23 22:11:31 +0000

    Amended server port to static env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const homeRoutes = require('./routes/homeRoutes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
